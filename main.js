eeb9f68353fededb2b5cf8198d5673b7bc9a1a52
Monika Prokorym <monika.prokorym@gmail.com>
2023-02-26 11:38:49 +0100

    funkcjonalność działań

---
+++ main.js
@@ -0,0 +1,79 @@
+const previousNumber = document.querySelector(".prevNum");
+const currentNumber = document.querySelector(".currNum");
+const mathSign = document.querySelector('.mathSign');
+const resultNumber = document.querySelector('#result');
+const numbersButtons = document.querySelectorAll('.number');
+const operatorsButtons = document.querySelectorAll('.operator');
+const equalsButton = document.querySelector('.equals');
+
+
+let result = 0;
+
+function displayNumbers () {
+
+    resultNumber.classList.add("hidden");
+
+    if(this.textContent === '.' && currentNumber.innerHTML.includes('.')) return;
+    if(this.textContent === '.' && currentNumber.innerHTML === '') return currentNumber.innerHTML = '0.'
+
+    currentNumber.innerHTML += this.textContent;
+}
+
+function operate () {
+
+    if(currentNumber.innerHTML === '' && resultNumber.innerHTML === '' && this.textContent === '-') {
+        currentNumber.innerHTML = '-';
+        return;
+    } else if(resultNumber.innerHTML !== ''){
+        resultNumber.classList.add("hidden");
+        currentNumber.innerHTML = result;
+    } else if(currentNumber.innerHTML === ''){
+    return;
+    } if(mathSign.innerHTML !== '') {
+       showResult();
+        currentNumber.innerHTML = result;
+    }
+    previousNumber.innerHTML = currentNumber.innerHTML;
+    mathSign.innerHTML = this.textContent;
+    currentNumber.innerHTML = '';
+}
+
+function showResult () {
+    if(previousNumber.innerHTML === '' && currentNumber.innerHTML === '') return;
+
+    let a = +(currentNumber.innerHTML);
+    let b = +(previousNumber.innerHTML);
+    let operator = mathSign.innerHTML;
+
+    switch(operator){
+        case '+':
+            result = a + b;
+            break;
+        case '-':
+            result = b - a;
+            break;
+        case 'x':
+            result = a * b;
+            break;
+        case '/':
+            result = b / a;
+            break;
+    }
+}
+
+numbersButtons.forEach((button) => {
+    button.addEventListener('click', displayNumbers)
+});
+
+operatorsButtons.forEach((button) => {
+    button.addEventListener('click', operate)
+});
+
+equalsButton.addEventListener('click', () => {
+    showResult();
+    currentNumber.innerHTML = '';
+    previousNumber.innerHTML = '';
+    mathSign.innerHTML = '';
+    resultNumber.classList.remove("hidden");
+    resultNumber.innerHTML = result;
+});
