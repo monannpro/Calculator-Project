5023c357ef1fd1dff1df067cf40a16430fb459a1
Monika Prokorym <monika.prokorym@gmail.com>
2023-02-26 13:47:37 +0100

    Warunki na liczby dziesiętne i ujemne. Bez zaokrągleń.

--- main.js
+++ main.js
@@ -6,13 +6,46 @@ const numbersButtons = document.querySelectorAll('.number');
 const operatorsButtons = document.querySelectorAll('.operator');
 const equalsButton = document.querySelector('.equals');
 
-let result;
+let result = '';
 
 function displayNumbers () {
-    currentNumber.innerHTML = this.textContent;
+    //jeśli mamy wynik i kolejno naciśniey cyfrę bez operatora- wyczyści poprzedni wynik i zresetuje liczenie
+    if(resultNumber !== '') {
+        resultNumber.innerHTML = '';
+    }
+    //jeśli liczba zawiera juz . przerwij
+    if(this.textContent === '.' && currentNumber.innerHTML.includes('.')) return;
+    //jeśli nie ma liczby to po wcisnieciu . pokazuje 0.
+    if(this.textContent === '.' && currentNumber.innerHTML === '') return currentNumber.innerHTML = '0.'
+
+
+    currentNumber.innerHTML += this.textContent;
 }
 
 function operate () {
+    //jeśli nie ma liczby ani wyniku, po naciśnięciu - zwróć -
+    if(currentNumber.innerHTML === '' && resultNumber.innerHTML === '' && this.textContent === '-') {
+        currentNumber.innerHTML = '-';
+        return;
+    }
+
+    //jeśli wynik już istnieje, schowaj wysnik, wyczyść wynik, przypisz wynik do num2
+    if(resultNumber.innerHTML !== ''){
+        resultNumber.classList.add("hidden");
+        resultNumber.innerHTML = '';
+        currentNumber.innerHTML = result;
+
+    } //w przeciwnym razie (jeśli wynik nie istnieje) i nie ma liczby przerwij.
+    else if (currentNumber.innerHTML === '') {
+        return;
+    }
+
+    //jeśli operator już użyty, podlicz i jako num2 pokaż wynik
+    if(mathSign.innerHTML !== ''){
+        showResult();
+        resultNumber.innerHTML = '';
+        currentNumber.innerHTML = result;
+    }
 
     previousNumber.innerHTML = currentNumber.innerHTML;
     mathSign.innerHTML = this.textContent;
@@ -20,11 +53,15 @@ function operate () {
 }
 
 function showResult () {
+    //nie licz jeśli pierwsza lub druga liczba będą puste
+    if(previousNumber.innerHTML === '' || currentNumber.innerHTML === '') return;
 
+    //zmienne
     let a = +(previousNumber.innerHTML);
     let b = +(currentNumber.innerHTML);
     let operator = mathSign.innerHTML;
 
+    //operacje
     switch(operator) {
         case '+':
             result = a + b;
@@ -36,11 +73,17 @@ function showResult () {
             result = a * b;
             break;
         case '/':
-            result = a / b;
-            break;
+            if(b === 0){
+                result = 'Error';
+            } else {
+                result = a / b;
+                break;
+            }
     }
 }
 
+
+//nasłuchiwania
 numbersButtons.forEach((button) => {
     button.addEventListener('click', displayNumbers)
 });
@@ -50,9 +93,11 @@ operatorsButtons.forEach((button) => {
 });
 
 equalsButton.addEventListener('click', () => {
+    //policz, wyczyść num1, num2 i znak. Pokaż wynik.
     showResult();
     previousNumber.innerHTML = '';
     currentNumber.innerHTML = '';
     mathSign.innerHTML = '';
+    resultNumber.classList.remove("hidden");
     resultNumber.innerHTML = result;
 });
