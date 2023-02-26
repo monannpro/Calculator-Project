7e8a2dbca2e7610ba89bc747a9dbaeda3cc7bb51
Monika Prokorym <monika.prokorym@gmail.com>
2023-02-26 12:39:48 +0100

    Podstawowe działania (+, -, /, *) bez warunków na liczby dziesiętne i ujemne.

--- main.js
+++ main.js
@@ -6,57 +6,37 @@ const numbersButtons = document.querySelectorAll('.number');
 const operatorsButtons = document.querySelectorAll('.operator');
 const equalsButton = document.querySelector('.equals');
 
-
-let result = 0;
+let result;
 
 function displayNumbers () {
-
-    resultNumber.classList.add("hidden");
-    if(mathSign.innerHTML === '') resultNumber.innerHTML = '';
-    if(this.textContent === '.' && (currentNumber.innerHTML.includes('.') || resultNumber.innerHTML.includes('.'))) return;
-    if(this.textContent === '.' && currentNumber.innerHTML === '') return currentNumber.innerHTML = '0.';
-
-    currentNumber.innerHTML += this.textContent;
+    currentNumber.innerHTML = this.textContent;
 }
 
 function operate () {
 
-    if(currentNumber.innerHTML === '' && resultNumber.innerHTML === '' && this.textContent === '-') {
-        currentNumber.innerHTML = '-';
-        return;
-    } else if(resultNumber.innerHTML === '' && currentNumber.innerHTML === ''){
-        return;
-    } else if(resultNumber.innerHTML !== ''){
-        resultNumber.classList.add("hidden");
-        currentNumber.innerHTML = result;
-    }  if(mathSign.innerHTML !== '') {
-       showResult();
-        currentNumber.innerHTML = result;
-    }
     previousNumber.innerHTML = currentNumber.innerHTML;
     mathSign.innerHTML = this.textContent;
     currentNumber.innerHTML = '';
 }
 
 function showResult () {
-    if(previousNumber.innerHTML === '' && currentNumber.innerHTML === '') return;
 
-    let a = +(currentNumber.innerHTML);
-    let b = +(previousNumber.innerHTML);
+    let a = +(previousNumber.innerHTML);
+    let b = +(currentNumber.innerHTML);
     let operator = mathSign.innerHTML;
 
-    switch(operator){
+    switch(operator) {
         case '+':
             result = a + b;
             break;
         case '-':
-            result = b - a;
+            result = a - b;
             break;
         case 'x':
             result = a * b;
             break;
         case '/':
-            result = b / a;
+            result = a / b;
             break;
     }
 }
@@ -71,9 +51,8 @@ operatorsButtons.forEach((button) => {
 
 equalsButton.addEventListener('click', () => {
     showResult();
-    currentNumber.innerHTML = '';
     previousNumber.innerHTML = '';
+    currentNumber.innerHTML = '';
     mathSign.innerHTML = '';
-    resultNumber.classList.remove("hidden");
     resultNumber.innerHTML = result;
 });
